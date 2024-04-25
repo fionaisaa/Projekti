1f7886ab101ccd1eb727f2e4d50407010a5207f6
fionaisaa <fionaisa04@gmail.com>
2024-04-25 21:59:46 +0200

    regjistrimi i nje studenti

--- digitalschoolproject/src/main/java/com/plc/digitalschoolproject/students/StudentController.java
+++ digitalschoolproject/src/main/java/com/plc/digitalschoolproject/students/StudentController.java
@@ -1,15 +1,34 @@
 package com.plc.digitalschoolproject.students;
 
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 
 
 @RestController
 public class StudentController {
 
+    
+    private StudentService studentService;
+
+    public StudentController( final StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+
+    //e merr nje student me id te caktuar
+    @GetMapping(path = "/students/{id}")
+    public StudentEntity getStudentById(@PathVariable(name = "id") Long id){
+
+        return studentService.findById(id);
+    }
+
+
+    // e merr nje list
     @GetMapping(path = "/students")
     public String getStudentName(){
 
@@ -17,8 +36,9 @@ public class StudentController {
     }
 
     @PostMapping(path = "/students")
-    public void createStudent(){
+    public StudentEntity createStudent(@RequestBody StudentEntity studentEntity){
 
+        return studentService.save(studentEntity);
     }
 
     @DeleteMapping(path = "/students")

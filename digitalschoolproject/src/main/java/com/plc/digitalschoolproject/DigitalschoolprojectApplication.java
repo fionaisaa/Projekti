859b58c20d1dc7f5144295d6f1a84644d1e63901
fionaisaa <fionaisa04@gmail.com>
2024-04-25 20:34:09 +0200

    files

---
+++ digitalschoolproject/src/main/java/com/plc/digitalschoolproject/DigitalschoolprojectApplication.java
@@ -0,0 +1,13 @@
+package com.plc.digitalschoolproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalschoolprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalschoolprojectApplication.class, args);
+	}
+
+}
---
+++ digitalschoolproject/src/test/java/com/plc/digitalschoolproject/DigitalschoolprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plc.digitalschoolproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalschoolprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
